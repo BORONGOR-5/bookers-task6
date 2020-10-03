43a958d3c75d43386396424565197a305ec8df73
EC2 Default User <ec2-user@ip-172-31-3-30.ec2.internal>
2020-10-03 10:17:21 +0000

    develop-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :introduction,  length: { maximum: 50 }
   
   has_many :books,  dependent: :destroy
-  
+  has_many :post_comments, dependent: :destroy
   # validates :name,  presence: true, length: { minimum: 2, maximum: 20 }
   
   # validates :introduction,  length: { maximum: 50 }
